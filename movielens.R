c3804c1f6d69f434f990c6258813b2a9fda44592
dodemabiteniwe <dodemabiteniwe@gmail.com>
2024-09-23 11:02:22 +0000

    update and new file

--- movielens.R
+++ movielens.R
@@ -1,7 +1,83 @@
 ##########################################################
+# Create edx and final_holdout_test sets 
+##########################################################
+
+# Note: this process could take a couple of minutes
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+
+library(tidyverse)
+library(caret)
+
+# MovieLens 10M dataset:
+# https://grouplens.org/datasets/movielens/10m/
+# http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+options(timeout = 120)
+
+dl <- "ml-10M100K.zip"
+if(!file.exists(dl))
+  download.file("https://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+
+ratings_file <- "ml-10M100K/ratings.dat"
+if(!file.exists(ratings_file))
+  unzip(dl, ratings_file)
+
+movies_file <- "ml-10M100K/movies.dat"
+if(!file.exists(movies_file))
+  unzip(dl, movies_file)
+
+ratings <- as.data.frame(str_split(read_lines(ratings_file), fixed("::"), simplify = TRUE),
+                         stringsAsFactors = FALSE)
+colnames(ratings) <- c("userId", "movieId", "rating", "timestamp")
+ratings <- ratings %>%
+  mutate(userId = as.integer(userId),
+         movieId = as.integer(movieId),
+         rating = as.numeric(rating),
+         timestamp = as.integer(timestamp))
+
+movies <- as.data.frame(str_split(read_lines(movies_file), fixed("::"), simplify = TRUE),
+                        stringsAsFactors = FALSE)
+colnames(movies) <- c("movieId", "title", "genres")
+movies <- movies %>%
+  mutate(movieId = as.integer(movieId))
+
+movielens <- left_join(ratings, movies, by = "movieId")
+
+# Final hold-out test set will be 10% of MovieLens data
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+#set.seed(1) # if using R 3.5 or earlier
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+# Make sure userId and movieId in final hold-out test set are also in edx set
+final_holdout_test <- temp %>% 
+  semi_join(edx, by = "movieId") %>%
+  semi_join(edx, by = "userId")
+
+# Add rows removed from final hold-out test set back into edx set
+removed <- anti_join(temp, final_holdout_test)
+edx <- rbind(edx, removed)
+
+rm(dl, ratings, movies, test_index, temp, movielens, removed)
+save(edx, file = "rdas/edx.rda")
+save(final_holdout_test, file = "rdas/final_holdout_test.rda")
+
+
+
+
+#------------------------------ANALYSIS------------------------------------------------------------------------------------
+
+
+
+##########################################################
 # Exploratory analysis
 ##########################################################
-# Load the necessary library
+
+
+# Load the necessary library and dataset
 library(tidyverse)
 library(caret)
 library(Matrix)
@@ -9,7 +85,6 @@ library(grDevices)
 library(dplyr)
 library(ggplot2)
 library(cowplot)
-#library(data.table)
 library(ggthemes)
 library(lubridate)
 library(Metrics)
@@ -18,13 +93,27 @@ library(scales)
 library(stringr)
 library(tibble)
 library(tidyr)
-#if(!require(devtools)) install.packages("devtools")
-#devtools::install_github("kassambara/ggpubr")
+if(!require(devtools)) install.packages("devtools")
+devtools::install_github("kassambara/ggpubr")
 library(ggpubr)
+library("recommenderlab")
+library(knitr)
+library(kableExtra)
 
-load("rdas/edx.rda")
-load("rdas/final_holdout_test.rda")
+load("../rdas/edx.rda")
+load("../rdas/final_holdout_test.rda")
 edx$timestamp <- as.POSIXct(edx$timestamp, origin = "1970-01-01")
+final_holdout_test$timestamp <- as.POSIXct(final_holdout_test$timestamp, origin = "1970-01-01")
+
+# First lines of edx data
+kable(
+  head(edx,5), booktabs = TRUE, caption = "First lines of edx data.")%>%
+  kable_styling(latex_options = c("HOLD_position","scale_down"))
+# First lines of validation data
+kable(
+  head(final_holdout_test,5), booktabs = TRUE, caption = "First lines of validation data.")%>%
+  kable_styling(latex_options = c("HOLD_position","scale_down"))
+
 
 #####################################################################
 # Sparsity of MovieLens Ratings Matrix
@@ -46,10 +135,10 @@ image(x = seq_len(ncol(full_matrix)), y = seq_len(nrow(full_matrix)), z = t(full
       xlab = "Users", ylab = "Movies",
       col = "yellow", breaks = c(min(full_matrix, na.rm = TRUE), max(full_matrix, na.rm = TRUE)),
       axes = TRUE, # Enable axes for clarity
-      xaxt = 'n', yaxt = 'n')+ abline(h = 0:100 + 0.5, v = 0:100 + 0.5, col = "grey")# Hide axis ticks
+      xaxt = 'n', yaxt = 'n')
 axis(1, at = seq(0,101,10), labels = TRUE)
 axis(2, at = seq(0,101,10), labels = TRUE)
-#save(murders, file = "rdas/murders.rda")
+
 
 ################################################################################
 #Distributions of variables
@@ -67,7 +156,6 @@ edx %>%
   scale_y_continuous(labels = comma) +
   scale_x_continuous(n.breaks = 10) +
   theme_economist()
-#ggsave("figs/barplot.png")
 
 ######## Rating per movies ########
 plot_mov1 <- edx%>%
@@ -80,8 +168,7 @@ plot_mov1 <- edx%>%
   xlab("Movies") +
   ylab("Number of ratings") +
   scale_y_continuous(labels = comma) +
-  scale_x_continuous(n.breaks = 10) +
-  theme_economist() 
+  scale_x_continuous(n.breaks = 10)
 ########### Movies' rating histogram ###################
 plot_mov2 <- edx %>%
   group_by(movieId) %>%
@@ -92,8 +179,7 @@ plot_mov2 <- edx %>%
   xlab("Rating count") +
   ylab("Number of movies") +
   scale_y_continuous(labels = comma) +
-  scale_x_log10(n.breaks = 10) +
-  theme_economist()
+  scale_x_log10(n.breaks = 10)
 
 ggarrange(plot_mov1, plot_mov2,  
           labels = c("A", "B"),
@@ -110,8 +196,7 @@ plot_user1 <- edx%>%
   xlab("Users") +
   ylab("Number of ratings") +
   scale_y_continuous(labels = comma) +
-  scale_x_continuous(n.breaks = 10) +
-  theme_economist()
+  scale_x_continuous(n.breaks = 10)
 ########### Users' rating histogram ###################
 plot_user2 <- edx %>%
   group_by(userId) %>%
@@ -122,8 +207,7 @@ plot_user2 <- edx %>%
   xlab("Rating count") +
   ylab("Number of users") +
   scale_y_continuous(labels = comma) +
-  scale_x_log10(n.breaks = 10) +
-  theme_economist()
+  scale_x_log10(n.breaks = 10)
 
 ggarrange(plot_user1, plot_user2,  
           labels = c("A", "B"),
@@ -138,8 +222,6 @@ ggarrange(plot_user1, plot_user2,
 ###############################################"
 # Modeling approach A: Popular, random, svd algorithms with recommendalab
 ############################
-
-library("recommenderlab")
 
 # Convert the data to a realRatingMatrix
 rat_mat <- as(select(edx,userId,movieId,rating), "realRatingMatrix")
@@ -162,14 +244,8 @@ evaluate_models <- function(train, test, models) {
   results <- list()
   
   for (model_name in models) {
-    cat("Evaluating model:", model_name, "\n")
-    
     model <- Recommender(train, method = model_name)
     prediction <- predict(model, test, type = "ratingMatrix")
-    
-    #rmse <- calcPredictionAccuracy(prediction, test, byUser = FALSE)$RMSE
-    #mse <- calcPredictionAccuracy(prediction, test, byUser = FALSE)$MSE
-    #mae <- calcPredictionAccuracy(prediction, test, byUser = FALSE)$MAE
     
     results[[model_name]] <- calcPredictionAccuracy(prediction, test, byUser = FALSE)
   }
@@ -185,7 +261,9 @@ evaluation <- tibble(Model = c("RANDOM", "POPULAR", "SVD"),
                      MSE = c(results$RANDOM[[2]], results$POPULAR[[2]], results$SVD[[2]]),
                      RMSE = c(results$RANDOM[[1]], results$POPULAR[[1]], results$SVD[[1]]))
 
-print(evaluation)
+kable(
+  evaluation, booktabs = TRUE, caption = "Algorithms performance.")%>%
+  kable_styling(latex_options = "HOLD_position")
 
 ###############################################"
 # Modeling approach B: Linear Regression Model
@@ -259,7 +337,7 @@ lambdas_tibble %>%
 
 lambda <- lambdas[which.min(lambdas_rmse)]
 
- mu <- mean(train_set$rating)
+mu <- mean(train_set$rating)
 
 b_i_regularized <- train_set %>% 
   group_by(movieId) %>%
@@ -283,8 +361,10 @@ evaluation <-bind_rows(evaluation,
                               MAE  = Metrics::mae(test_set$rating, y_hat_regularized),
                               MSE  = Metrics::mse(test_set$rating, y_hat_regularized),
                               RMSE = Metrics::rmse(test_set$rating, y_hat_regularized)))
- 
-print(evaluation)
+
+kable(
+  evaluation, booktabs = TRUE, caption = "Algorithms performance.")%>%
+  kable_styling(latex_options = "HOLD_position")
 
 ###############################################"
 # Modeling approach C: Matrix factorization
@@ -294,63 +374,18 @@ print(evaluation)
 
 set.seed(1)
 train_rec <- with(train_set, data_memory(user_index = userId, 
-                                                item_index = movieId,
-                                                rating     = rating))
+                                         item_index = movieId,
+                                         rating     = rating))
 test_rec <- with(test_set, data_memory(user_index = userId, 
-                                              item_index = movieId, 
-                                              rating     = rating))
-valid_rec <- with(final_holdout_test, data_memory(user_index = userId, 
                                        item_index = movieId, 
                                        rating     = rating))
+
 ##### Model objet#######
 
 recom_system <- Reco()
 
 #### Model is tuned ######
 tun <- recom_system$tune(train_rec, opts = list(dim = c(10, 20, 30),
-                                                                   lrate = c(0.1, 0.2),
-                                                                   nthread  = 4,
-                                                                   niter = 10))
-
-
-##### Model training #############
-
-recom_system$train(train_rec, opts = c(tun$min,
-                                                       nthread = 4,
-                                                       niter = 31))
-
-#####Prediction##############
-
-y_hat_MatFac <-  recom_system$predict(test_rec, out_memory())
-y_hat_MatFac2 <-  recom_system$predict(valid_rec, out_memory())
-
-Metrics::rmse(final_holdout_test$rating, y_hat_MatFac2)
-#############Evaluation ###############
-
-evaluation <- bind_rows(evaluation,
-                        tibble(Model = "Matrix factorization",
-                               MAE  = Metrics::mae(test_set$rating, y_hat_MatFac),
-                               MSE  = Metrics::mse(test_set$rating, y_hat_MatFac),
-                               RMSE = Metrics::rmse(test_set$rating, y_hat_MatFac)))
-print(evaluation)
-knitr::kable(evaluation)
-save(evaluation, file = "rdas/evaluation.rda")
-save(tun2, file = "rdas/tun2.rda")
-
-#######################################################################
-#Model performance on the final holdout data
-######################################################
-
-set.seed(1)
-train_rec2 <- with(edx, data_memory(user_index = userId, 
-                                         item_index = movieId,
-                                         rating     = rating))
-
-valid_rec <- with(final_holdout_test, data_memory(user_index = userId, 
-                                                  item_index = movieId, 
-                                                  rating     = rating))
-#### Model is tuned ######
-tun2 <- recom_system$tune(train_rec2, opts = list(dim = c(10, 20, 30),
                                                 lrate = c(0.1, 0.2),
                                                 nthread  = 4,
                                                 niter = 10))
@@ -358,14 +393,79 @@ tun2 <- recom_system$tune(train_rec2, opts = list(dim = c(10, 20, 30),
 
 ##### Model training #############
 
-recom_system$train(train_rec2, opts = c(tun2$min,
+recom_system$train(train_rec, opts = c(tun$min,
                                        nthread = 4,
                                        niter = 31))
 
 #####Prediction##############
-y_hat_MatFac2 <-  recom_system$predict(valid_rec, out_memory())
+y_hat_MatFac <-  recom_system$predict(test_rec, out_memory())
 
+#############Evaluation ###############
+evaluation <- bind_rows(evaluation,
+                        tibble(Model = "Matrix factorization",
+                               MAE  = Metrics::mae(test_set$rating, y_hat_MatFac),
+                               MSE  = Metrics::mse(test_set$rating, y_hat_MatFac),
+                               RMSE = Metrics::rmse(test_set$rating, y_hat_MatFac)))
+kable(
+  evaluation, booktabs = TRUE, caption = "Algorithms performance on test data.")%>%
+  kable_styling(latex_options = "HOLD_position")
+
+#######################################################################
+#Model performance on the final holdout data
+######################################################
+set.seed(1)
+train_rec2 <- with(edx, data_memory(user_index = userId, 
+                                    item_index = movieId,
+                                    rating     = rating))
+valid_rec <- with(final_holdout_test, data_memory(user_index = userId, 
+                                                  item_index = movieId, 
+                                                  rating     = rating))
+#### Model is tuned ######
+tun2 <- recom_system$tune(train_rec2, opts = list(dim = c(10, 20, 30),
+                                                  lrate = c(0.1, 0.2),
+                                                  nthread  = 4,
+                                                  niter = 10))
+
+##### Model training #############
+recom_system$train(train_rec2, opts = c(tun2$min,
+                                        nthread = 4,
+                                        niter = 31))
+
+#####Prediction##############
+y_hat_MatFac2 <-  recom_system$predict(valid_rec, out_memory())
 Metrics::rmse(final_holdout_test$rating, y_hat_MatFac2)
+
+##############################################################
+# Top 10 movie recommendation by the final model
+########################################################
+top10_pred <- tibble(Title = final_holdout_test$title, y_hat = y_hat_MatFac2) %>%
+  arrange(desc(y_hat)) %>%
+  select(Title) %>%
+  unique() %>%
+  slice_head(n = 10)
+top10_pred_df <- data.frame(Title = top10_pred,
+                            Rating = rep(NA, 10),
+                            Count = rep(NA, 10))
+
+for (i in 1:10) {
+  ind <- which(final_holdout_test$title == as.character(top10_pred[i,]))
+  top10_pred_df$Rating[i] <- mean(final_holdout_test$rating[ind])
+  top10_pred_df$Count[i] <- sum(
+    final_holdout_test$title == as.character(top10_pred[i,])
+  )
+}
+kable(
+  top10_pred_df, booktabs = TRUE, caption = "Top 10 movie recommendation by the final model.")%>%
+  kable_styling(latex_options = "HOLD_position")
+
+#-------------------------------------End.----------------------------------------------------------------------------------
+
+
+
+
+
+
+
 
 
 
